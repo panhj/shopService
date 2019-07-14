fd4474e233fb7e221cd9533740fe4bddd7914874
panhj <1159688878@qq.com>
2019-07-14 22:40:48 +0800

    fitst commit

---
+++ src/main/java/com/shopproject/App.java
@@ -0,0 +1,41 @@
+package com.shopproject;
+
+import com.shopproject.dao.UserDOMapper;
+import com.shopproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(scanBasePackages = {"com.shopproject"})
+@RestController
+@MapperScan("com.shopproject.dao")
+public class App 
+{
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @RequestMapping("/")
+    public String Home()
+    {
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null) {
+            return "no user";
+        } else {
+            return userDO.getName();
+        }
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
